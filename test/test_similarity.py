0f0e4ce91d569aa0aa1d1bec6d090d4ba52326d8
yossi <yossico@rnd-hub.com>
2018-09-12 11:41:48 +0300

    ref

---
+++ test/test_similarity.py
@@ -0,0 +1,11 @@
+#!nlp/bin/python
+
+import spacy
+
+nlp = spacy.load('en_core_web_md')  # make sure to use larger model!
+# tokens = nlp(u'dog cat banana')
+tokens = nlp(u'king queen sultan ceasar')
+
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
