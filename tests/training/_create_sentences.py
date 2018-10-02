163ed796051eb5d607244e5c4253cb624c0952f3
yossi <yossico@rnd-hub.com>
2018-10-02 10:38:31 +0300

    ref

--- tests/training/_create_sentences.py
+++ tests/training/_create_sentences.py
@@ -10,42 +10,48 @@ model = 'en'
 nlp = spacy.load(model, disable=['parser', 'ner'])
 
 _this_script_dir = os.path.dirname(os.path.abspath(__file__))
-fname = os.path.join(_this_script_dir, 'horses')
-word = 'horse'
-# fname = os.path.join(_this_script_dir, 'cats')
-# word = 'cat'
 
-lines = []
-with open(fname) as f:
-    content = f.readlines()
-    for line in content:
-        if len(line) < 10:
-            continue
-        lines.append(line)
-    with open(fname+'.py', 'w') as f2:
-        f2.write(u"sentences = [\n")
-        for sentence in lines:
-            sentence = sentence.strip('\r\n')
+_label = 'ANIMAL'
 
+_fname = os.path.join(_this_script_dir, 'horses')
+_word = 'horse'
+
+# _fname = os.path.join(_this_script_dir, 'cats')
+# _word = 'cat'
+
+# open input file
+with open(_fname) as f_in:
+    # create output file
+    with open(_fname + '.py', 'w') as f_out:
+
+        # convert input - line by line
+        for line in f_in:
+            # skip irrelevant lines
+            if len(line) < 10:
+                continue
+
+            # line clenup
+            sentence = line.strip('\r\n')
+
+            # process sentence
             doc = nlp(sentence)
             offset = length = -1
             for t in doc:
-                if t.lemma_ == word:
+                if (t.orth_ == _word or t.lemma_ == _word):
                     offset = t.idx
-                    length = len(t.norm_)
+                    length = len(t.lemma_)
                     break
             if offset < 0 or length <= 0:
                 continue
 
-            entities = [(offset, offset+length, 'ANIMAL')]
+            entities = [(offset, offset+length, _label)]
             tags = biluo_tags_from_offsets(doc, entities)
-            assert tags == ['O', 'O', 'U-LOC', 'O']
 
             escaped = sentence.replace("'", "\\'")
             escaped = escaped.replace("\"", "\\\"")
 
-            f2.write(u'\t("{}", {{\n'.format(escaped))
-            f2.write(u"\t\t'entities': [({}, {}, 'ANIMAL')]\n"
-                     .format(offset, length))
-            f2.write(u"\t}),\n\n")
-        f2.write(u"]")
+            f_out.write(u'\t("{}", {{\n'.format(escaped))
+            f_out.write(u"\t\t'entities': [({}, {}, {})]\n"
+                        .format(offset, length, _label))
+            f_out.write(u"\t}),\n\n")
+        f_out.write(u"]")
