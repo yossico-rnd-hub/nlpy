421dd4046cb4c29e6fbdf2b1bf6c324382f59028
Yossi Cohen <yossi.cohen@live.com>
2018-10-25 21:51:35 +0300

    ref

--- nlp/relations/x_en_person_org.py
+++ nlp/relations/x_en_person_org.py
@@ -15,35 +15,30 @@ class EN_REL_PERSON_ORG(object):
 
     def __call__(self, doc, relations):
         for person in filter(lambda e: e.label_ == 'PERSON', doc.ents):
-            # lilox
-            # for t in self.person_verb_org(doc, person):
-            #     relations.append(create_relation(*t))
-            # for t in self.person_rights_org(doc, person):
-            #     relations.append(create_relation(*t))
-            # for t in self.person_lefts_org(doc, person):
-            #     relations.append(create_relation(*t))
             for t in self.person_prep_org(doc, person):
+                relations.append(create_relation(*t))
+            for t in self.person_lefts_org(doc, person):
+                relations.append(create_relation(*t))
+            # TODO
+            for t in self.person_rights_org(doc, person):
+                relations.append(create_relation(*t))
+            # TODO
+            for t in self.person_verb_org(doc, person):
                 relations.append(create_relation(*t))
         return doc
 
-    def person_verb_org(self, doc, person):
-        # TODO
-        # if (person[0].head != person[0]):
-        #     head_verb = person[0].head
-        #     while True:
-        #         if (head_verb.pos_ == 'VERB'):
-        #             break
-        #         head_verb = head_verb.head
-        return []
-
-    def person_rights_org(self, doc, person):
-        # TODO
-        pred = next(filter(lambda w: w.pos_ in (
-            'NOUN', 'VERB'), person.rights), None)
-        if pred:
-            pred = doc[pred.i:pred.i+1]
-            for org in filter(lambda w: w.ent_type_ == 'ORG', pred.rights):
-                yield (person, pred, org)
+    def person_prep_org(self, doc, person):
+        # e.g: '... authored by Peter W. Battaglia of Google's DeepMind'
+        prep = next(filter(lambda w: w.dep_ == 'prep', person.rights), None)
+        if prep:
+            org_iob = []
+            for org in filter(lambda w: w.ent_type_ == 'ORG', prep.subtree):
+                if (org.ent_iob > 0):
+                    org_iob.append(org)
+                    if (doc[org.i+1].ent_iob > 0):
+                        continue
+                    org = doc[org_iob[0].i:org_iob[-1].i + 1]
+                yield (person, None, org)
 
     def person_lefts_org(self, doc, person):
         # e.g: 'NYU professor Gary Marcus'
@@ -54,11 +49,21 @@ class EN_REL_PERSON_ORG(object):
             for org in filter(lambda w: w.ent_type_ == 'ORG', pred.lefts):
                 yield (person, pred, org)
 
-    def person_prep_org(self, doc, person):
-        # e.g: '... authored by Peter W. Battaglia of Google's DeepMind'
-        prep = next(filter(lambda w: w.dep_ == 'prep', person.subtree), None)
-        if prep:
-            # print('lilo ---------------', list(prep.rights))
-            # pred = doc[prep.i:prep.i+1]
-            for org in filter(lambda w: w.ent_type_ == 'ORG', prep.subtree):
-                yield (person, None, org)
+    def person_rights_org(self, doc, person):
+        # TODO
+        # pred = next(filter(lambda w: w.pos_ in (
+        #     'NOUN', 'VERB'), person.rights), None)
+        # if pred:
+        #     pred = doc[pred.i:pred.i+1]
+        #     for org in filter(lambda w: w.ent_type_ == 'ORG', pred.rights):
+        #         yield (person, pred, org)
+
+    def person_verb_org(self, doc, person):
+        # TODO
+        # if (person[0].head != person[0]):
+        #     head_verb = person[0].head
+        #     while True:
+        #         if (head_verb.pos_ == 'VERB'):
+        #             break
+        #         head_verb = head_verb.head
+        return []
